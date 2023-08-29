a244b840b11969472600df312c7797db411eb856
Irving Lop <irving.dev.mobi@gmail.com>
2023-08-29 16:32:09 -0600

    testing Compose Multiplatform with a simple button.

--- androidApp/src/main/java/dev/irving/composemp/android/MainActivity.kt
+++ androidApp/src/main/java/dev/irving/composemp/android/MainActivity.kt
@@ -3,24 +3,17 @@ package dev.irving.composemp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.irving.composemp.Greeting
+import dev.irving.composemp.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
+                App()
             }
         }
     }
---
+++ shared/src/iosMain/kotlin/dev/irving/composemp/App.kt
@@ -0,0 +1,5 @@
+package dev.irving.composemp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
